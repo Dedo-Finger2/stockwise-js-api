2a29847dd82cdb6c61b83c47ffb42f6c61002f00
Antonio Mauricio <antonioimportant@gmail.com>
2024-07-18 20:11:43 -0300

    feat: add check missing required params utils func

---
+++ src/lib/check-missing-required-params.js
@@ -0,0 +1,17 @@
+/**
+ * @param {Array<string>} requiredParams
+ * @param {Object} object
+ */
+export function checkMissingRequiredParams(requiredParams, object) {
+  const missingParams = [];
+
+  for (const [key, value] of Object.entries(object)) {
+    if (!value) {
+      missingParams.push(key);
+    }
+  }
+
+  if (missingParams.length) {
+    throw new TypeError(`Missing required params: ${missingParams.join(", ")}`);
+  }
+}
